fee73faec5cabbc3eb0543bd70dc6a7ae9f50749
mxkproductions <mxkproductions@mxkproductions.mxkproductions>
2022-03-02 20:46:48 -0500

    Modified Shell Scripts and Added More Screenshots for README.md

--- test_files/del-test-entries.sh
+++ test_files/del-test-entries.sh
@@ -2,5 +2,5 @@
 # File Type: DB Cleanup
 # Author: Marcus X. Kielman
 # Description: Deletes Test Entries from Kubernetes Container
-export POD_NAME=$(kubectl get pods -o name | grep mariadb)
-echo "DELETE FROM payments WHERE checkNumber='HR141523'; DELETE FROM customers WHERE customerNumber=100;" | kubectl exec -i $POD_NAME -- mysql -uroot -proot classicmodels
+export POD_NAME=$(kubectl get pods -o name --context kind-devops-api | grep mariadb)
+echo "DELETE FROM payments WHERE checkNumber='HR141523'; DELETE FROM customers WHERE customerNumber=100;" | kubectl exec -i $POD_NAME --context kind-devops-api -- mysql -uroot -proot classicmodels
--- test_files/kube-run.sh
+++ test_files/kube-run.sh
@@ -3,7 +3,7 @@
 # Author: Marcus X. Kielman
 # Description: Opens Port Forwarding for API in Kubernetes, and runs Tests
 #!/bin/bash
-kubectl port-forward service/devops-api 9090:9090 --address "192.168.1.245" &
-sleep 5s
+kubectl port-forward service/devops-api 9090:9090 --address "192.168.1.201" --context kind-devops-api &
+sleep 5s 
 python3 test_files/kube_test.py
 ./test_files/del-test-entries.sh       #Deletes Test Entries from Database
