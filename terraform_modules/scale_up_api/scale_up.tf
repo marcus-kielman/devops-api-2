1427ace80408b5a79fcf1a587178d32af001a924
mxkproductions <mxkproductions@mxkproductions.mxkproductions>
2022-03-03 09:40:25 -0500

    Modified terraform

--- terraform_modules/scale_up_api/scale_up.tf
+++ terraform_modules/scale_up_api/scale_up.tf
@@ -1,30 +1,63 @@
-resource "kubernetes_replication_controller" "devops-api" {
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+variable "client_certificate" {
+  type = string
+}
+
+variable "client_key" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+provider "kubernetes" {
+  host = var.host
+
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+}
+resource "kubernetes_deployment" "devops-api" {
   metadata {
     name = "devops-api"
     labels = {
-      test = "devops-api"
+      App = "devops-api"
     }
   }
 
   spec {
-    selector = {
-      test = "devops-api"
+    replicas = 5
+    selector {
+      match_labels = {
+        App = "devops-api"
+      }
     }
     template {
       metadata {
         labels = {
-          test = "devops-api"
-        }
-        annotations = {
-          "key1" = "value1"
+          App = "devops-api"
         }
       }
-
       spec {
-        replicas = 5
         container {
           image = "marcuskielman/devops_api:latest"
           name  = "devops-api"
+
+          port {
+            container_port = 8081
+          }
 
           resources {
             limits = {
@@ -39,5 +72,22 @@ resource "kubernetes_replication_controller" "devops-api" {
         }
       }
     }
+  }
+}
+resource "kubernetes_service" "devops-api" {
+  metadata {
+    name = "devops-api"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.devops-api.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 9090
+      target_port = 8081
+    }
+
+    type = "NodePort"
   }
 }
